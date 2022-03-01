bc6969a30ab5b1caac115bc62b03d84198870f11
Tyler Hall <tyler@hyper.io>
2022-03-01 16:04:33 -0500

    fix!: resolve hyper error and bubble rejected non-handled exceptions. #12
    various bug fixes and error mappings
    
    BREAKING CHANGE - caught exceptions are resolved, not rejected, as hyper
    errors
    
    The only time a method on the adapter should return a rejected promise
    is in the case of an unhandled exception.
    
    Aligns with discussion detailed in:
    https://github.com/hyper63/journal/blob/master/design-docs/003-errors.md

--- adapter_test.js
+++ adapter_test.js
@@ -12,6 +12,7 @@ import {
   removeDocPath,
   updateDocPath,
 } from "./paths.js";
+import { moveUnderscoreId } from "./utils.js";
 
 const headers = createHeaders("admin", "password");
 
@@ -41,6 +42,10 @@ const stubResponse = (status, body) => {
 
 const fetch = spy(() => Promise.resolve(response));
 
+const cleanup = () => {
+  fetch.calls.splice(0, fetch.calls.length);
+};
+
 const adapter = createAdapter({
   config: { origin: ES },
   asyncFetch: asyncFetch(fetch),
@@ -54,7 +59,7 @@ Deno.test("remove index", async () => {
 
   const result = await adapter.deleteIndex(INDEX);
 
-  assertObjectMatch(fetch.calls.shift(), {
+  assertObjectMatch(fetch.calls.pop(), {
     args: [deleteIndexPath(ES, INDEX), {
       method: "DELETE",
       headers,
@@ -62,6 +67,26 @@ Deno.test("remove index", async () => {
   });
 
   assertEquals(result.ok, true);
+
+  cleanup();
+});
+
+Deno.test("remove index - not found", async () => {
+  // remove index
+  stubResponse(404, {
+    error: {
+      type: "index_not_found_exception",
+      reason: "foo",
+    },
+  });
+
+  const result = await adapter.deleteIndex(INDEX);
+
+  assertEquals(result.ok, false);
+  assertEquals(result.status, 404);
+  assertEquals(result.msg, `index ${INDEX} not found`);
+
+  cleanup();
 });
 
 Deno.test("create index", async () => {
@@ -73,7 +98,7 @@ Deno.test("create index", async () => {
     mappings: { fields: ["title"] },
   });
 
-  assertObjectMatch(fetch.calls.shift(), {
+  assertObjectMatch(fetch.calls.pop(), {
     args: [createIndexPath(ES, INDEX), {
       method: "PUT",
       headers,
@@ -82,6 +107,52 @@ Deno.test("create index", async () => {
   });
 
   assertEquals(result.ok, true);
+
+  cleanup();
+});
+
+Deno.test("create index - maps _id", async () => {
+  // create index
+  stubResponse(201, { ok: true });
+
+  const result = await adapter.createIndex({
+    index: INDEX,
+    mappings: { fields: ["_id", "title"] },
+  });
+
+  assertObjectMatch(fetch.calls.pop(), {
+    args: [createIndexPath(ES, INDEX), {
+      method: "PUT",
+      headers,
+      body:
+        '{"mappings":{"properties":{"title":{"type":"text"},"__movedUnderscoreId63__":{"type":"text"}}}}',
+    }],
+  });
+
+  assertEquals(result.ok, true);
+
+  cleanup();
+});
+
+Deno.test("create index - error", async () => {
+  // create index
+  stubResponse(404, {
+    error: {
+      type: "mapper_parsing_exception",
+      reason: "foo",
+    },
+  });
+
+  const result = await adapter.createIndex({
+    index: INDEX,
+    mappings: { fields: ["title"] },
+  });
+
+  assertEquals(result.ok, false);
+  assertEquals(result.status, 422);
+  assertEquals(result.msg, `failed to parse mapping for index ${INDEX}: foo`);
+
+  cleanup();
 });
 
 Deno.test("index document", async () => {
@@ -94,7 +165,7 @@ Deno.test("index document", async () => {
     doc: DOC1,
   });
 
-  assertObjectMatch(fetch.calls.shift(), {
+  assertObjectMatch(fetch.calls.pop(), {
     args: [indexDocPath(ES, INDEX, DOC1.id), {
       method: "PUT",
       headers,
@@ -103,6 +174,58 @@ Deno.test("index document", async () => {
   });
 
   assertEquals(result.ok, true);
+
+  cleanup();
+});
+
+Deno.test("index document - error", async () => {
+  // index doc
+  stubResponse(404, {
+    error: {
+      type: "index_not_found_exception",
+      reason: "foo",
+    },
+  });
+
+  const result = await adapter.indexDoc({
+    index: INDEX,
+    key: DOC1.id,
+    doc: DOC1,
+  });
+
+  assertEquals(result.ok, false);
+  assertEquals(result.status, 404);
+  assertEquals(result.msg, `index ${INDEX} not found`);
+
+  cleanup();
+});
+
+Deno.test("index document - maps _id", async () => {
+  // index doc
+  stubResponse(200, { ok: true });
+
+  const withUnderscoreId = {
+    ...DOC1,
+    _id: DOC1.id,
+  };
+
+  const result = await adapter.indexDoc({
+    index: INDEX,
+    key: DOC1.id,
+    doc: withUnderscoreId,
+  });
+
+  assertObjectMatch(fetch.calls.pop(), {
+    args: [indexDocPath(ES, INDEX, DOC1.id), {
+      method: "PUT",
+      headers,
+      body: JSON.stringify(moveUnderscoreId(withUnderscoreId)),
+    }],
+  });
+
+  assertEquals(result.ok, true);
+
+  cleanup();
 });
 
 Deno.test("get document", async () => {
@@ -114,7 +237,7 @@ Deno.test("get document", async () => {
     key: DOC1.id,
   });
 
-  assertObjectMatch(fetch.calls.shift(), {
+  assertObjectMatch(fetch.calls.pop(), {
     args: [getDocPath(ES, INDEX, DOC1.id), {
       method: "GET",
       headers,
@@ -123,6 +246,50 @@ Deno.test("get document", async () => {
 
   assertEquals(result.doc.title, DOC1.title);
   assertEquals(result.ok, true);
+
+  cleanup();
+});
+
+Deno.test("get document - error doc not found", async () => {
+  // get doc
+  stubResponse(404, {
+    error: {
+      type: "resource_not_found_exception",
+      reason: "foo",
+    },
+  });
+
+  const result = await adapter.getDoc({
+    index: INDEX,
+    key: DOC1.id,
+  });
+
+  assertEquals(result.ok, false);
+  assertEquals(result.status, 404);
+  assertEquals(result.msg, `document at key ${DOC1.id} not found`);
+
+  cleanup();
+});
+
+Deno.test("get document - error index not found", async () => {
+  // get doc
+  stubResponse(404, {
+    error: {
+      type: "index_not_found_exception",
+      reason: "foo",
+    },
+  });
+
+  const result = await adapter.getDoc({
+    index: INDEX,
+    key: DOC1.id,
+  });
+
+  assertEquals(result.ok, false);
+  assertEquals(result.status, 404);
+  assertEquals(result.msg, `index ${INDEX} not found`);
+
+  cleanup();
 });
 
 Deno.test("update document", async () => {
@@ -138,7 +305,7 @@ Deno.test("update document", async () => {
     },
   });
 
-  assertObjectMatch(fetch.calls.shift(), {
+  assertObjectMatch(fetch.calls.pop(), {
     args: [updateDocPath(ES, INDEX, DOC1.id), {
       method: "PUT",
       headers,
@@ -150,7 +317,11 @@ Deno.test("update document", async () => {
   });
 
   assertEquals(result.ok, true);
+
+  cleanup();
 });
+
+// update document sad paths covered by other tests
 
 Deno.test("delete document", async () => {
   // remove doc
@@ -161,7 +332,7 @@ Deno.test("delete document", async () => {
     key: DOC1.id,
   });
 
-  assertObjectMatch(fetch.calls.shift(), {
+  assertObjectMatch(fetch.calls.pop(), {
     args: [removeDocPath(ES, INDEX, DOC1.id), {
       method: "DELETE",
       headers,
@@ -169,6 +340,29 @@ Deno.test("delete document", async () => {
   });
 
   assertEquals(result.ok, true);
+
+  cleanup();
+});
+
+Deno.test("delete document - 404 passthrough", async () => {
+  // remove doc
+  stubResponse(404, { ok: true });
+
+  const result = await adapter.removeDoc({
+    index: INDEX,
+    key: "not_found",
+  });
+
+  assertObjectMatch(fetch.calls.pop(), {
+    args: [removeDocPath(ES, INDEX, "not_found"), {
+      method: "DELETE",
+      headers,
+    }],
+  });
+
+  assertEquals(result.ok, true);
+
+  cleanup();
 });
 
 Deno.test("bulk", async () => {
@@ -188,7 +382,7 @@ Deno.test("bulk", async () => {
     ],
   });
 
-  assertObjectMatch(fetch.calls.shift(), {
+  assertObjectMatch(fetch.calls.pop(), {
     args: [bulkPath(ES), {
       method: "POST",
       headers,
@@ -198,6 +392,119 @@ Deno.test("bulk", async () => {
 
   assertEquals(result.ok, true);
   assert(result.results);
+
+  cleanup();
+});
+
+Deno.test("bulk - maps all _ids and operations", async () => {
+  // bulk operation
+  stubResponse(200, {
+    items: [
+      { index: { _id: DOC1.id, ...DOC1 } },
+      { index: { _id: DOC2.id, ...DOC2 } },
+    ],
+  });
+
+  const doc1WithUnderscoreId = { ...DOC1, _id: DOC1.id };
+  await adapter.bulk({
+    index: INDEX,
+    docs: [
+      doc1WithUnderscoreId,
+      DOC2,
+    ],
+  });
+
+  const call = fetch.calls.pop();
+  assertObjectMatch(call, {
+    args: [bulkPath(ES), {
+      method: "POST",
+      headers,
+    }],
+  });
+
+  let { args: [, { body }] } = call;
+
+  body = body.split("\n");
+  console.log(body);
+  body = body.slice(0, -1);
+  const [index1, first, index2] = body;
+
+  assertEquals(doc1WithUnderscoreId._id, JSON.parse(index1).index._id);
+  assertEquals(DOC2.id, JSON.parse(index2).index._id);
+
+  assertEquals(first, JSON.stringify(moveUnderscoreId(doc1WithUnderscoreId)));
+
+  cleanup();
+});
+
+Deno.test("bulk - entire request error", async () => {
+  // bulk operation
+  stubResponse(404, {
+    error: {
+      type: "index_not_found_exception",
+      reason: "foo",
+    },
+  });
+
+  const result = await adapter.bulk({
+    index: INDEX,
+    docs: [
+      DOC1,
+      DOC2,
+    ],
+  });
+
+  assertEquals(result.ok, false);
+  assertEquals(result.status, 404);
+  assertEquals(result.msg, `index ${INDEX} not found`);
+
+  cleanup();
+});
+
+Deno.test("bulk - with errors", async () => {
+  // bulk operation
+  stubResponse(200, {
+    items: [
+      { index: { _id: DOC1.id, ...DOC1 } },
+      {
+        index: {
+          _id: DOC2.id,
+          ...DOC2,
+          status: 400,
+          error: { type: "resource_already_exists_exception", reason: "foo" },
+        },
+      },
+    ],
+  });
+
+  const result = await adapter.bulk({
+    index: INDEX,
+    docs: [
+      DOC1,
+      DOC2,
+    ],
+  });
+
+  assertObjectMatch(fetch.calls.pop(), {
+    args: [bulkPath(ES), {
+      method: "POST",
+      headers,
+      // TODO: Tyler. Assert body here eventually
+    }],
+  });
+
+  assertEquals(result.ok, true);
+  assert(result.results);
+  const [success, err] = result.results;
+
+  assert(success.ok);
+  assert(success.id);
+
+  assertEquals(err.status, 409);
+  assertEquals(err.msg, `document at key ${DOC2.id} already exists`);
+  assertEquals(err.ok, false);
+
+  cleanup();
 });
 
 Deno.test("query", async () => {
@@ -205,7 +512,8 @@ Deno.test("query", async () => {
   stubResponse(200, {
     hits: {
       hits: [
-        DOC1,
+        { _source: DOC1 },
+        { _source: moveUnderscoreId({ ...DOC2, _id: DOC2.id }) },
       ],
     },
   });
@@ -221,7 +529,7 @@ Deno.test("query", async () => {
     },
   });
 
-  assertObjectMatch(fetch.calls.shift(), {
+  assertObjectMatch(fetch.calls.pop(), {
     args: [queryPath(ES, INDEX), {
       method: "POST",
       headers,
@@ -231,5 +539,38 @@ Deno.test("query", async () => {
 
   assertEquals(result.ok, true);
   assert(result.matches);
-  assertEquals(result.matches.length, 1);
+  assertEquals(result.matches.length, 2);
+
+  assertEquals(result.matches[0].id, DOC1.id);
+  // maps _id back
+  assertEquals(result.matches[1]._id, DOC2.id);
+
+  cleanup();
+});
+
+Deno.test("query - error", async () => {
+  // query docs
+  stubResponse(404, {
+    error: {
+      type: "index_not_found_exception",
+      reason: "foo",
+    },
+  });
+
+  const result = await adapter.query({
+    index: "movies",
+    q: {
+      query: "gatsby",
+      fields: ["title"],
+      filter: {
+        rating: 4,
+      },
+    },
+  });
+
+  assertEquals(result.ok, false);
+  assertEquals(result.status, 404);
+  assertEquals(result.msg, `index ${INDEX} not found`);
+
+  cleanup();
 });
