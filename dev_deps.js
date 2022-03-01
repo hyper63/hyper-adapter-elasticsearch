14c3211ee312736bc9ea19cc79cc9d3c8ef95bd7
Tyler Hall <tyler@hyper.io>
2022-03-01 16:01:47 -0500

    chore: bump deps

--- dev_deps.js
+++ dev_deps.js
@@ -3,5 +3,5 @@ export {
   assertEquals,
   assertObjectMatch,
   assertThrows,
-} from "https://deno.land/std@0.117.0/testing/asserts.ts";
-export { spy } from "https://deno.land/x/mock@0.12.1/mod.ts";
+} from "https://deno.land/std@0.127.0/testing/asserts.ts";
+export { spy } from "https://deno.land/x/mock@0.13.0/mod.ts";
